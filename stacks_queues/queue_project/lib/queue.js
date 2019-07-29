7414f04d1a72d761f5ad92b2390c9c29bd608036
Selam Weldu <ssweldu@gmail.com>
2019-07-28 21:19:23 -0700

    implement queue adt with enqueue, dequeue and size methods

--- stacks_queues/queue_project/lib/queue.js
+++ stacks_queues/queue_project/lib/queue.js
@@ -21,11 +21,52 @@
 // -----------
 
 class Node {
-
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
 }
 
 class Queue {
+    constructor(){
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
 
+    enqueue(value){
+        let newNode = new Node(value)
+        if(!this.front){
+            this.front = newNode;
+            this.back = newNode;
+        } else{
+            this.back.next = newNode;
+            this.back = newNode
+        }
+
+        return ++this.length
+    }
+
+    dequeue(){
+        if(!this.front){
+            return null
+        } else if(this.length === 1){
+            let removed = this.front
+            this.front = null;
+            this.back = null;
+            --this.length;
+            return removed.value
+        } else{
+            let removed = this.front;
+            this.front = this.front.next;
+            --this.length;
+            return removed.value
+        }
+    }
+
+    size(){
+        return this.length
+    }
 }
 
 exports.Node = Node;
