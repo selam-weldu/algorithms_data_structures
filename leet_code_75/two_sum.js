fbf877358361671a365d5e9309036161b514a1b8
Selam Weldu <ssweldu@gmail.com>
2019-08-12 22:09:57 -0700

    Solve twoSum in O(n)

--- leet_code_75/two_sum.js
+++ leet_code_75/two_sum.js
@@ -1,15 +1,33 @@
-let twoSum = (nums, target) => {
-    let map = {};
+// time: O(n)
+//space: O(n)
 
+
+let twoSum = function (nums, target) {
+    let map = {}
     for (let i = 0; i < nums.length; i++) {
-        let another = target - nums[i];
 
-        if (another in map) {
-            return [map[another], i];
-        }
+        let match = target - nums[i]
 
-        map[nums[i]] = i;
+        if (match in map) return [i, map[match]]
+
+        map[nums[i]] = i
+
     }
-
-    return null;
+    return null
 };
+
+// let twoSum = (nums, target) => {
+//     let map = {};
+
+//     for (let i = 0; i < nums.length; i++) {
+//         let another = target - nums[i];
+
+//         if (another in map) {
+//             return [map[another], i];
+//         }
+
+//         map[nums[i]] = i;
+//     }
+
+//     return null;
+// };
