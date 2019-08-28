1bf6c5053da4e6337a999e7fbbc7fc7555285e79
Selam Weldu <ssweldu@gmail.com>
2019-08-28 18:57:13 -0400

    Implement valid anagrams

---
+++ blind75/strings/valid_anagrams.js
@@ -0,0 +1,28 @@
+var isAnagram = function (s, t) {
+    // init hashMap
+    // add chars of first string into hashMap char as key and count as value
+    // iterate over second string
+    // check if char2 is in hashMap
+    // if no, return false
+    // if yes, subtract 1 from value
+    // outside of loop, return true if all values in hashmap are 0
+   
+    let hashMap = {};
+
+    for(let char of s){
+        hashMap[char] = ++hashMap[char] || 1;
+    }
+
+    for(let char of t){
+        if(char in hashMap){
+            hashMap[char] -= 1;
+        } else {
+            return false
+        }
+    }
+
+    let count = Object.values(hashMap);
+
+    return count.every(ele => ele === 0);
+
+};
