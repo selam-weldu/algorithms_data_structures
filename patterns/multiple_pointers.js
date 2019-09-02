49f56b99158dc519bfb1d9293bf17d9bb0dbdb0e
Selam Weldu <ssweldu@gmail.com>
2019-09-02 18:23:18 -0400

    Implement countUniqueValues

--- patterns/multiple_pointers.js
+++ patterns/multiple_pointers.js
@@ -23,4 +23,48 @@ function sumZero(arr){
     return []
 }
 
-console.log(sumZero([-2,0,2,4,5]));
+// console.log(sumZero([-2,0,2,4,5]));
+
+//arr is sorted
+function countUniqueValues(arr){
+    // start counter at 1 if arr is not empty
+    // init two pointer
+    // check if arr[i] !== arr[j]
+    // if yes, increment counter
+    // return counter at the end
+    if(arr.length === 0) return 0
+
+    let count = 1
+    for (let i = 0; i < arr.length - 1; i++) {
+        let j = i + 1;
+
+        if(arr[i] !== arr[j]){
+            ++count;
+        }
+    }
+
+    return count
+}
+
+// 7
+// console.log(countUniqueValues([1,2,3,4,4,7,7,7,12,12,13]));
+     
+// mutates arr
+function countUniqueValues2(arr){
+    // two pointer
+    // i = 0 and j loops the rest of the arr
+    // at each iteration check if arr[i] !== arr[j]
+    // if yes, ++i and arr[i] = arr[j]
+    // return i + 1, because index starts at 0
+
+    let i = 0
+    for (let j = 0; j < arr.length; j++) {
+        if(arr[i] !== arr[j]){
+            ++i;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+
+// console.log(countUniqueValues2([1,2,3,4,4,7,7,7,12,12,13]));
