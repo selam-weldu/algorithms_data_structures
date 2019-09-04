9992f59580cf9137331518c4898011ec030c1a9e
Selam Weldu <ssweldu@gmail.com>
2019-09-04 16:16:45 -0400

    Implement groupAnagrams

--- blind75/array/group_anagrams.js
+++ blind75/array/group_anagrams.js
@@ -1,14 +1,19 @@
 const groupAnagrams = function (strs) {
+    // init map, and keep sorted str as keys and array with anagrams as values
+    // for each str of strs, sort the str
+    // if it's not in map, make it a key and it's value []
+    // if in map push str value array
+    // return values of map
+    
     let map = {};
 
     for (str of strs) {
-        let k = str.split('').sort().join('')
+        let k = str.split('').sort().join('');
 
         if (!map[k]) {
             map[k] = [];
-
         }
-        map[k].push(str)
+        map[k].push(str);
     }
 
     return Object.values(map);
