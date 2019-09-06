b3f381641b7dee1948400f8d8c8ac6404313ed8e
Selam Weldu <ssweldu@gmail.com>
2019-09-06 15:26:24 -0400

    Practice using multiple pointer pattern

--- patterns/multiple_pointers.js
+++ patterns/multiple_pointers.js
@@ -118,3 +118,11 @@ function isSubsequence(str1, str2) {
     }
     return true
 }
+
+// Recurssive
+function isSubsequence(str1, str2) {
+    if (str1.length === 0) return true
+    if (str2.length === 0) return false
+    if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+    return isSubsequence(str1, str2.slice(1))
+}
