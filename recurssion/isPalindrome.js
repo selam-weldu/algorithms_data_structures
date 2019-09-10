3667d673fd8378c50a34fae8b0a8ad92e7e9c40c
Selam Weldu <ssweldu@gmail.com>
2019-09-09 20:10:01 -0400

    Implement isPalindrome recurssively

---
+++ recurssion/isPalindrome.js
@@ -0,0 +1,10 @@
+function isPalindrome(str) {
+    if (str.length <= 1) return true;
+
+    let first = str[0],
+        last = str[str.length - 1];
+
+    if (first !== last) return false;
+
+    return isPalindrome(str.slice(1, str.length - 1));
+}
