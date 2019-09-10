7711b37ea0d371171359b90a4c9061c0966ec017
Selam Weldu <ssweldu@gmail.com>
2019-09-09 20:05:12 -0400

    Implement reverseString recurssively

--- recurssion/reverse.js
+++ recurssion/reverse.js
@@ -1,11 +1,11 @@
-function reverse(str) {
+function reverseString(str) {
     let reversed = [];
 
     function _reverse(s) {
         if (!s.length) return;
 
         reversed.push(s[s.length - 1]);
-        
+
         _reverse(s.slice(0, s.length - 1))
     }
 
