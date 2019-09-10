787b36f4893f2ce012f074a775956797d6aa3ec7
Selam Weldu <ssweldu@gmail.com>
2019-09-09 20:54:38 -0400

    Implement nestedEvenSum recurssively

---
+++ recurssion/nested_even_sum.js
@@ -0,0 +1,40 @@
+function nestedEvenSum(obj){
+    let sum = 0;
+
+    function _nested(hash){
+        for(let key in hash){
+            if(typeof hash[key] === 'object'){
+                _nested(hash[key])
+            }else if(typeof hash[key] === 'number' && hash[key] % 2 === 0){
+                sum += hash[key];
+            }
+        }
+    }
+
+    _nested(obj)
+
+    return sum;
+}
+
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: "yup"
+        }
+    }
+}
+
+var obj2 = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: 'car' }
+};
+
+nestedEvenSum(obj1); // 6
+nestedEvenSum(obj2); // 10
