63f1018668e3c90c7da93fa58352421bb458271e
Selam Weldu <ssweldu@gmail.com>
2019-09-09 20:20:19 -0400

    Implement someRecursive

--- recurssion/is_odd.js
+++ recurssion/is_odd.js
@@ -1,9 +1,10 @@
 // Takes in an array and a callback
 // returns true if a single value in the array is true when passed to callback
-function someRecursive(arr, cb) {
+
+function someRecursive(arr, callback) {
     if (!arr.length) return false;
 
     if (cb(arr[0])) return true;
 
-    return someRecursive(arr.slice(1), cb);
+    return someRecursive(arr.slice(1), callback);
 }
