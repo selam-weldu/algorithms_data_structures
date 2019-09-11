8fa08a003c2015871f01cc79a0947087ca596ea7
Selam Weldu <ssweldu@gmail.com>
2019-09-11 15:45:46 -0400

    Implement nestedEvenSum

--- recurssion/nested_even_sum.js
+++ recurssion/nested_even_sum.js
@@ -16,6 +16,18 @@ function nestedEvenSum(obj){
     return sum;
 }
 
+// Pure recurssion
+function nestedEvenSum(obj, sum = 0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object') {
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
+
 var obj1 = {
     outer: 2,
     obj: {
