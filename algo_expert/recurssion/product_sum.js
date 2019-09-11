e4dcfc687b83feaad3d19481d374d09e319fe58a
Selam Weldu <ssweldu@gmail.com>
2019-09-11 18:08:26 -0400

    Implement productSum

---
+++ algo_expert/recurssion/product_sum.js
@@ -0,0 +1,15 @@
+// Time: O(n), n is number of ele in array
+// Space: O(d), maximum depth of special array
+
+function productSum(array, multiplier = 1) {
+    let sum = 0;
+
+    for (let ele of array) {
+        if (Array.isArray(ele)) {
+            sum += productSum(ele, multiplier + 1)
+        } else {
+            sum += ele
+        }
+    }
+    return sum * m;
+}
