2e0e899e29237031a3e97b8c6683cbedccb88b17
Selam Weldu <ssweldu@gmail.com>
2019-09-11 15:34:18 -0400

    Implement collectStrings

--- recurssion/collect_strings.js
+++ recurssion/collect_strings.js
@@ -1,3 +1,4 @@
+// helper function method
 function collectStrings(obj) {
     // iterate obj and if value is string add to arr
     // if it's an object call helper function again
@@ -15,5 +16,19 @@ function collectStrings(obj) {
     }
 
     _collect(obj)
+    return arr;
+}
+
+// pure recurssion
+function collectStrings(obj){
+    let arr = [];
+
+    for(let key in obj){
+        if(typeof obj[key] === 'object' && !Array.isArray(obj[key])){
+            arr = arr.concat(collectStrings(obj[key]))
+        } else if(typeof obj[key] === 'string'){
+            arr.push(obj[key])
+        }
+    }
     return arr;
 }
