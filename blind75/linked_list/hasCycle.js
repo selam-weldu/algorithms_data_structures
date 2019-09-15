8fdba6660a6f0610b69fa20afb3ed1d3a3931031
Selam Weldu <ssweldu@gmail.com>
2019-09-15 00:16:50 -0400

    Implement hasCycle

--- blind75/linked_list/hasCycle.js
+++ blind75/linked_list/hasCycle.js
@@ -4,36 +4,19 @@ function hasCycle(head) {
     // second pointer is two nodes ahead
     // loop while there is a second and second.next
     // inside loop check if first === second, return true if equal
-    if (!head || !head.next) return false;
+    if (!head || !head.next || !head.next.next) return false;
 
-    let first = head;
-    let second = head.next;
+    let first = head.next;
+    let second = head.next.next;
 
     while(first !== second){
         first = first.next;
-        if(!second || !second.next) return false;
+        if(!second.next || !second.next.next) return false;
         second = second.next.next;
     }
-
-    // while (second && second.next) {
-    //     if (first === second) return true
-    //     first = first.next;
-    //     second = second.next.next;
-    // }
-
+ 
     return true;
 }
 
-// short and sweet
-function hasCycle(head) {
-
-    while (head) {
-        if (head.visited) { return true }
-        head.visited = true
-        head = head.next
-    }
-
-    return false
-};
 
 
