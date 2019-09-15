14f8a0588e3c4eafb5fd8e3e1db12db5ac124359
Selam Weldu <ssweldu@gmail.com>
2019-09-14 23:23:42 -0400

    Implement findLoop

---
+++ algo_expert/linked_list/find_loop.js
@@ -0,0 +1,18 @@
+function findLoop(head) {
+    let first = head.next;
+    let second = head.next.next;
+
+    while (first !== second) {
+        first = first.next;
+        second = second.next.next;
+    }
+
+    first = head;
+
+    while (first !== second) {
+        first = first.next;
+        second = second.next;
+    }
+
+    return first;
+}
