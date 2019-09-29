176d6123ee8cd09cee9947c5000e88cbcfd46181
Selam Weldu <ssweldu@gmail.com>
2019-09-28 20:41:44 -0400

    Implement tree traversals

---
+++ algo_expert/BST/traversals.js
@@ -0,0 +1,34 @@
+function inOrderTraverse(tree, array) {
+    // Write your code here.
+    // 	left,self,right
+    if (!tree) return array;
+
+    if (tree.left) inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    if (tree.right) inOrderTraverse(tree.right, array);
+
+    return array;
+}
+
+function preOrderTraverse(tree, array) {
+    // Write your code here.
+    if (!tree) return array;
+
+    array.push(tree.value);
+    if (tree.left) preOrderTraverse(tree.left, array);
+    if (tree.right) preOrderTraverse(tree.right, array);
+
+    return array;
+}
+
+function postOrderTraverse(tree, array) {
+    // Write your code here.
+
+    if (!tree) return array;
+
+    if (tree.left) postOrderTraverse(tree.left, array);
+    if (tree.right) postOrderTraverse(tree.right, array);
+    array.push(tree.value);
+
+    return array;
+}
