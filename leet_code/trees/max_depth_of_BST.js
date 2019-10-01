73e4ad0020a58ac216546a0730ece8eb84ccb4e2
Selam Weldu <ssweldu@gmail.com>
2019-09-30 22:56:24 -0400

    Implement maxDepthOfBinaryTree

--- leet_code/trees/max_depth_of_BST.js
+++ leet_code/trees/max_depth_of_BST.js
@@ -37,3 +37,16 @@ const maxDepth = function (root) {
     if (!root) return 0
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
+
+/////
+// if we are counting edges not nodes
+
+const maxDepth = function(root){
+    if(!root) return -1;
+
+    if(!root.left && !root.right){
+        return 0;
+    }else{
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
