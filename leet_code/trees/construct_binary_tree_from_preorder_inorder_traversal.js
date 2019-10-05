1d769c24a2c760928193c540a313fb5fc148a380
Selam Weldu <ssweldu@gmail.com>
2019-10-05 19:54:19 -0400

    Implement buildTree

---
+++ leet_code/trees/construct_binary_tree_from_preorder_inorder_traversal.js
@@ -0,0 +1,36 @@
+// preorder[0] is the root node
+// left side of tree is everything to the left of preorder[0] in the inorder array
+// right side of tree is everything to the right of preorder[0]
+
+// O(n) time, O(n) space
+var buildTree = function (preorder, inorder) {
+    if (!inorder.length || !preorder.length) return null;
+
+    let node = new TreeNode(preorder[0]);
+
+    if (inorder.length === 1) return node; //optimization
+
+    let arr = divideArray(inorder, node.val),
+        left = arr[0],
+        right = arr[1];
+
+    node.left = buildTree(preorder.slice(1), left);
+    node.right = buildTree(preorder.slice(1 + left.length), right);
+
+    return node;
+};
+
+var divideArray = function (array, value) {
+    let left = [],
+        right = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            left = array.slice(0, i);
+            right = array.slice(i + 1);
+            break;
+        }
+    }
+
+    return [left, right];
+}
