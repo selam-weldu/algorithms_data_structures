bbb3ba52905a75e285b8f7fab34e3a1a326c3879
Selam Weldu <ssweldu@gmail.com>
2019-10-05 22:13:30 -0400

    Implement lowestCommonAncestor

--- leet_code/trees/lowest_common_ancestor.js
+++ leet_code/trees/lowest_common_ancestor.js
@@ -2,6 +2,7 @@
 // if lCA is smaller than both p and q so we move right
 // if it doesn't meet both checks return that node
 
+// O(h)
 var lowestCommonAncestor = (root, p, q) => {
 
     if (root === p || root === q || root === null) return root;
