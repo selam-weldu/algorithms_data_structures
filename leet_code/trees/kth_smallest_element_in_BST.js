a0d4129ddb541e3f861e6d4476b246f4cb5d7ba0
Selam Weldu <ssweldu@gmail.com>
2019-10-06 20:38:12 -0400

    Implement kthSmallest

--- leet_code/trees/kth_smallest_element_in_BST.js
+++ leet_code/trees/kth_smallest_element_in_BST.js
@@ -3,7 +3,8 @@ var kthSmallest = function (node, k) {
     let arr = [];
 
     function inorder(root) {
-        if (!root) return arr;
+        if (!root || arr.length === k) return;
+        
         if (root.left) inorder(root.left);
         arr.push(root.val);
         if (root.right) inorder(root.right);
