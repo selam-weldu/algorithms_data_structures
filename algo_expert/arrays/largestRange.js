2f1b384481002aaa0ccea612d9e60f430eb5adb2
Selam Weldu <ssweldu@gmail.com>
2019-10-10 00:28:23 -0400

    Implement largestRange

--- algo_expert/arrays/largestRange.js
+++ algo_expert/arrays/largestRange.js
@@ -5,46 +5,46 @@
 // exploring it's range
 // check all left and right and stop loops when element is not in hash
 
-// O(n) time - marking elements false in hash table keeps us 
+// O(n) time - marking elements false in hash table keeps us
 // from doing unnessary work
 // O(n) space
 function largestRange(array) {
-    let nums = {};
-    let range = [];
-    let largestRange = 0;
+  let nums = {};
+  let range = [];
+  let largestRange = 0;
 
-    for (let num of array) {
-        nums[num] = true;
+  for (let num of array) {
+    nums[num] = true;
+  }
+
+  for (let num of array) {
+    if (nums[num] === false) continue;
+
+    // 		lets explore
+    nums[num] === false; //******
+    let left = num - 1;
+    let right = num + 1;
+    let currentLength = 1; // ********
+
+    while (left in nums) {
+      nums[left] = false;
+      left -= 1;
+      ++currentLength;
     }
 
-    for (let num of array) {
-        if (nums[num] === false) continue;
-
-        // 		lets explore
-        nums[num] === false; //******
-        let left = num - 1;
-        let right = num + 1;
-        let currentLength = 1; // ********
-
-        while (left in nums) {
-            nums[left] = false;
-            left -= 1;
-            ++currentLength;
-        }
-
-        while (right in nums) {
-            nums[right] = false;
-            right += 1;
-            ++currentLength;
-        }
-
-        if (currentLength > largestRange) {
-            largestRange = currentLength;
-            range = [left + 1, right - 1] // ******
-        }
+    while (right in nums) {
+      nums[right] = false;
+      right += 1;
+      ++currentLength;
     }
 
-    return range;
+    if (currentLength > largestRange) {
+      largestRange = currentLength;
+      range = [left + 1, right - 1]; // ******
+    }
+  }
+
+  return range;
 }
 
 
