a067d07ad76315db4620f96bb8e422f4a86d70ec
Selam Weldu <ssweldu@gmail.com>
2019-10-10 22:37:13 -0400

    Implement numIslands

--- leet_code/graphs/number_of_islands.js
+++ leet_code/graphs/number_of_islands.js
@@ -1,32 +1,39 @@
+// 0(n) time , O(1) space
+
+// two loops to iterate over grid 
+// dfs on all values == 1
+// check edge cases so row and col are within bounds
+// change 1 into 0 so we don't revisit
 const numIslands = function(grid) {
-  let nIslands = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] == 1) {
-        nIslands += dfs(grid, i, j);
-      }
+  let numIslands = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] == 0) continue;
+      numIslands += traverse(row, col, grid);
     }
   }
 
-  return nIslands;
+  return numIslands;
 };
 
-function dfs(grid, i, j) {
+const traverse = function(row, col, grid) {
   if (
-    i < 0 ||
-    j < 0 ||
-    i >= grid.length ||
-    j >= grid[0].length ||
-    grid[i][j] == 0
+    row < 0 ||
+    col < 0 ||
+    row >= grid.length ||
+    col >= grid[0].length ||
+    grid[row][col] == 0
   ) {
     return 0;
   }
 
-  grid[i][j] = 0; // so we don't revisit
+  grid[row][col] = 0;
 
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
+  traverse(row - 1, col, grid);
+  traverse(row + 1, col, grid);
+  traverse(row, col + 1, grid);
+  traverse(row, col - 1, grid);
+
   return 1;
-}
+};
