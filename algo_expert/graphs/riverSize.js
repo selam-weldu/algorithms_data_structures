4c5fad9ce51e735d7c99956f1fdf51e5f3a7c803
Selam Weldu <ssweldu@gmail.com>
2019-10-13 17:51:33 -0400

    Implement riverSizes

--- algo_expert/graphs/riverSize.js
+++ algo_expert/graphs/riverSize.js
@@ -1,3 +1,4 @@
+// O(n) time, O(1) space
 function riverSizes(matrix) {
   let sizes = [];
 
@@ -45,3 +46,61 @@ function getNeighbors(i, j, matrix) {
 
   return neighbors;
 }
+
+
+
+// using an auxilary matrix to keep track of visited cells
+
+// O(n) time and space
+function riverSizes(matrix) {
+  let sizes = [];
+  let visited = matrix.map(row => row.map(el => false));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 1 && !visited[i][j]) {
+        traverse(i, j, matrix, sizes, visited);
+      }
+    }
+  }
+
+  return sizes;
+}
+
+function traverse(i, j, matrix, sizes, visited) {
+  let riverSize = 0;
+  let stack = [[i, j]];
+  let neighbors;
+
+  while (stack.length) {
+    [i, j] = stack.pop();
+
+    if (visited[i][j]) continue;
+    visited[i][j] = true;
+    if (matrix[i][j] === 0) continue;
+
+    ++riverSize;
+
+    neighbors = getNeighbors(i, j, matrix);
+
+    neighbors.forEach(neighbor => {
+      stack.push(neighbor);
+    });
+  }
+
+  if (riverSize) sizes.push(riverSize);
+}
+
+function getNeighbors(i, j, matrix) {
+  neighbors = [];
+
+  if (i > 0) neighbors.push([i - 1, j]);
+  if (j > 0) neighbors.push([i, j - 1]);
+  if (i < matrix.length - 1) neighbors.push([i + 1, j]);
+  if (j < matrix[0].length - 1) neighbors.push([i, j + 1]);
+
+  return neighbors;
+}
+
+// Do not edit the line below.
+exports.riverSizes = riverSizes;
