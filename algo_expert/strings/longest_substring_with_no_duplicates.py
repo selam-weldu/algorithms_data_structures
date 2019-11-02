82ab65b49119ee2de324728eaf647e2a6dabc820
Selam Weldu <ssweldu@gmail.com>
2019-11-01 21:56:58 -0400

    Implement longest_substring_with_no_duplicates

--- algo_expert/strings/longest_substring_with_no_duplicates.py
+++ algo_expert/strings/longest_substring_with_no_duplicates.py
@@ -7,10 +7,10 @@ def longestSubstringWithoutDuplication(string):
     seen = {}
 
     for i, char in enumerate(string):
-            if char in seen:
-                start_idx = max(start_idx, seen[char] + 1)
-            if longest[1] - longest[0] < i + 1 - start_idx:
-                longest = [start_idx, i + 1]
-            seen[char] = i
+        if char in seen:
+            start_idx = max(start_idx, seen[char] + 1)
+        if longest[1] - longest[0] < i + 1 - start_idx:
+            longest = [start_idx, i + 1]
+        seen[char] = i
 
 	return string[longest[0]: longest[1]]
