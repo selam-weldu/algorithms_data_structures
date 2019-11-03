b14758f33f2be568d5e26f1e698e5c447b2df4a4
Selam Weldu <ssweldu@gmail.com>
2019-11-03 14:43:42 -0500

    Implement detectCycle

--- leet_code/python/linked_list/detect_cycle.py
+++ leet_code/python/linked_list/detect_cycle.py
@@ -20,3 +20,22 @@ class Solution:
                 return slow
         return None
 
+'''
+    Consider the following linked list, where E is the cylce entry and X, the crossing point of fast and slow.
+        H: distance from head to cycle entry E
+        D: distance from E to X
+        L: cycle length
+                          _____
+                         /     \
+        head_____H______E       \
+                        \       /
+                         X_____/   
+        
+    
+        If fast and slow both start at head, when fast catches slow, slow has traveled H+D and fast 2(H+D). 
+        Assume fast has traveled n loops in the cycle, we have:
+        2H + 2D = H + D + L  -->  H + D = nL  --> H = nL - D
+        Thus if two pointers start from head and X, respectively, one first reaches E, the other also reaches E. 
+        In my solution, since fast starts at head.next, we need to move slow one step forward in the beginning of part 2
+'''
+
