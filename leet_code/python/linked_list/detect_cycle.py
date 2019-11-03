4e9f56cf9b619e60a6b7a0869f3e46086f10983b
Selam Weldu <ssweldu@gmail.com>
2019-11-02 23:40:45 -0400

    Implement detectCycle

---
+++ leet_code/python/linked_list/detect_cycle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                slow = head
+
+                while fast != slow:
+                    fast = fast.next
+                    slow = slow.next
+
+                return slow
+        return None
