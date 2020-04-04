618fbd0038f02197fe121f61f3e8e4ebb4775ffa
Selam Weldu <ssweldu@gmail.com>
2020-04-04 16:33:40 -0400

    Implement longest palindromic substring

--- leet_code/python/strings/longest_palindromic_substring.py
+++ leet_code/python/strings/longest_palindromic_substring.py
@@ -1,18 +1,24 @@
-# O(n) time, O(1) space
+# O(n^2) time, O(1) space
 def longestPalindromicSubstring(string):
-	currentLongest = [0, 1]
-	for i in range(1, len(string)):
+    if len(string) < 2:
+        return string
+
+	current_longest = [0, 0]
+	for i in range(len(string)):
 		odd = getPalindrome(string, i-1, i+1)
 		even = getPalindrome(string, i-1, i)
-		longest = max(odd, even, key=lambda x: x[1] - x[0])
-		currentLongest = max(longest, currentLongest, key=lambda x: x[1] - x[0])
-	return string[currentLongest[0]:currentLongest[1]]
+
+		longer = max(odd, even, key=lambda x: x[1] - x[0])
+		current_longest = max(current_longest, longer, key=lambda x: x[1] - x[0])
+
+	return string[current_longest[0]: current_longest[1] + 1]
 
 
-def getPalindrome(string, leftIdx, rightIdx):
-	while leftIdx >= 0 and rightIdx < len(string):
-		if string[leftIdx] != string[rightIdx]:
+def getPalindrome(string, left, right):
+	while left >= 0 and right < len(string):
+		if string[left] != string[right]:
 			break
-		leftIdx -= 1
-		rightIdx += 1
-	return [leftIdx + 1, rightIdx]
+		left -= 1
+		right += 1
+
+	return [left + 1, right - 1]
