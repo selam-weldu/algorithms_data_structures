9e515bbbcfa9e943a02b439046b0b5ad1ed1e49a
Selam Weldu <ssweldu@gmail.com>
2020-04-17 18:20:42 -0400

    Implement min coin change

--- leet_code/python/DP/house_robbers.py
+++ leet_code/python/DP/house_robbers.py
@@ -1,6 +1,6 @@
 # O(n) time, O(1) space
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+    def rob(self, nums):
         if not len(nums):
             return 0
         if len(nums) == 1:
@@ -15,16 +15,16 @@ class Solution:
         return current
 
 # O(n) time, O(n) space
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if not len(nums):
-            return 0
-        if len(nums) == 1:
-            return nums[0]
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         if not len(nums):
+#             return 0
+#         if len(nums) == 1:
+#             return nums[0]
 
-        grab = nums[:]
-        grab[1] = max(grab[1], grab[0])
+#         grab = nums[:]
+#         grab[1] = max(grab[1], grab[0])
 
-        for i in range(2,len(nums)):
-            grab[i] = max(grab[i - 1], nums[i] + grab[i - 2])
-        return grab[-1]
+#         for i in range(2,len(nums)):
+#             grab[i] = max(grab[i - 1], nums[i] + grab[i - 2])
+#         return grab[-1]
--- leet_code/python/DP/max_sum_no_adjacent.py
+++ leet_code/python/DP/max_sum_no_adjacent.py
@@ -1,33 +1,33 @@
 # O(n) time | O(1) space
 def maxSubsetSumNoAdjacent(array):
-    if not array:
-		return 0
-	if len(array) == 1:
-		return array[0]
+  if not array:
+      return 0
+  elif len(array) == 1:
+      return array[0]
 	
-	first = array[0]
-	second = max(array[0],array[1])
-	
-	for i in range(2,len(array)):
-		current = max(second, first + array[i])
-		first = second
-		second = current
-		
-	return second
+  first = array[0]
+  second = max(array[0],array[1])
+
+  for i in range(2,len(array)):
+      current = max(second, first + array[i])
+      first = second
+      second = current
+    
+  return second
 
 
 
 # O(n) time and space
-def maxSubsetSumNoAdjacent(array):
-	if not array:
-		return 0
-	if len(array) == 1:
-		return array[0]
+# def maxSubsetSumNoAdjacent(array):
+#     if not array:
+#         return 0
+#     if len(array) == 1:
+#         return array[0]
 
-	max_sum = array[:]
-	max_sum[1] = max(array[0],array[1])
-	
-	for i in range(2,len(array)):
-		max_sum[i] = max(max_sum[i-1], max_sum[i-2]+array[i])
-	return max_sum[-1]
+#     max_sum = array[:]
+#     max_sum[1] = max(array[0],array[1])
+
+#     for i in range(2,len(array)):
+#         max_sum[i] = max(max_sum[i-1], max_sum[i-2]+array[i])
+#     return max_sum[-1]
 
