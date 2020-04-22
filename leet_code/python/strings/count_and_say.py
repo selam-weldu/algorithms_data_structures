7f956e6c38aa70200e5524759f720e018d8379b0
Selam Weldu <ssweldu@gmail.com>
2020-04-21 21:27:16 -0400

    Implement string compression

--- leet_code/python/strings/count_and_say.py
+++ leet_code/python/strings/count_and_say.py
@@ -1,27 +1,32 @@
-# O(n2^n) time
-def next_number(self, number):
+# O(n^2) time | O(n) space
+class Solution:
+    def next_number(self, number):
         result = []
-        idx = 0
 
-        while idx < len(number):
-            count = 1
-            while idx + 1 < len(number) and number[idx] == number[idx + 1]:
-                idx += 1
-                count += 1
-            result.append(str(count) + number[idx])
-            idx += 1
+        i = j = 0
+        while i < len(number):
+            while (j < len(number) and number[i] == number[j]):
+                j += 1
+
+            count = j - i
+            result.append(str(count) + number[i])
+
+            i = j
 
         return "".join(result)
 
-def countAndSay(self, n: int) -> str:
-    if n == 0: return
-    if n == 1: return '1'
-    
-    s = '1'
-    for _ in range(1,n):
-        s = self.next_number(s)
-        
-    return s
+    def countAndSay(self, n: int) -> str:
+        if n == 0:
+            return
+        if n == 1:
+            return '1'
+
+        s = '1'
+        for _ in range(2, n+1):
+            s = self.next_number(s)
+
+        return s
+
 
 
 # 1.     1
@@ -29,3 +34,4 @@ def countAndSay(self, n: int) -> str:
 # 3.     21
 # 4.     1211
 # 5.     111221
+# 6.     312211
