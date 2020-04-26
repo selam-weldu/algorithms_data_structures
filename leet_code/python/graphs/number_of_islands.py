1e483b058fce21ad71fa155512f1388293a1cba5
Selam Weldu <ssweldu@gmail.com>
2020-04-25 21:17:42 -0400

    Implement river sizes

--- leet_code/python/graphs/number_of_islands.py
+++ leet_code/python/graphs/number_of_islands.py
@@ -1,23 +1,49 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) <= 0:
+        if not grid or len(grid) < 1:
             return 0
 
-        num_of_islands = 0
+        number_of_islands = 0
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if (grid[i][j] == '1'):
-                    num_of_islands += 1
+                if grid[i][j] == '1':
+                    number_of_islands += 1
                     self.sink(grid, i, j)
 
-        return num_of_islands
+        return number_of_islands
 
     def sink(self, grid, i, j):
-        if (i >= 0 and i < len(grid) and j >= 0 and j < len(grid[0]) and grid[i][j] == '1'):
-            grid[i][j] = '0'
+        if not((0 <= i < len(grid)) and (0 <= j < len(grid[0])) and grid[i][j] == '1'):
+            return
 
-            self.sink(grid, i + 1, j)
-            self.sink(grid, i - 1, j)
-            self.sink(grid, i, j + 1)
-            self.sink(grid, i, j - 1)
+        grid[i][j] = '0'
+
+        for node in self.get_neighbors(grid, i, j):
+            self.sink(grid, node[0], node[1])
+
+        # self.sink(grid, i + 1, j)
+        # self.sink(grid, i - 1, j)
+        # self.sink(grid, i, j + 1)
+        # self.sink(grid, i, j - 1)
+
+    def get_neighbors(self, matrix, row, col):
+        dr = [1, -1, 0, 0]
+        dc = [0, 0, 1, -1]
+
+        neighbors = []
+
+        for i in range(len(dr)):
+            r = row + dr[i]
+            c = col + dc[i]
+
+            if r < 0 or c < 0:
+                continue
+            if r >= len(matrix) or c >= len(matrix[0]):
+                continue
+            if matrix[r][c] == 0:
+                continue
+
+            neighbors.append([r, c])
+
+        return neighbors
--- leet_code/python/graphs/river_sizes.py
+++ leet_code/python/graphs/river_sizes.py
@@ -3,10 +3,10 @@ def riverSizes(matrix):
     sizes = []
 
 
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            if matrix[row][col] == 1:
-                sizes.append(count(matrix, row, col))
+for row in range(len(matrix)):
+		for col in range(len(matrix[0])):
+			if matrix[row][col] == 1:
+				sizes.append(count(matrix, row, col))
 
 	return sizes
 
@@ -16,4 +16,31 @@ def count(matrix, row,col):
 
 	matrix[row][col] = 0
 
-	return 1 + count(matrix, row+1,col) + count(matrix,row-1,col) + count(matrix,row,col+1) + count(matrix,row,col-1)
+	result = 1
+
+	for node in get_neighbors(matrix, row,col):
+		result += count(matrix, node[0],node[1])
+
+	return result
+
+	# return 1 + count(matrix,row+1,col) + count(matrix,row-1,col) + count(matrix,row,col+1) + count(matrix,row,col-1)
+
+def get_neighbors(matrix, row,col):
+	dr = [1, -1,0,0]
+	dc = [0, 0,1,-1]
+
+	neighbors = []
+
+	for i in range(len(dr)):
+		r = row + dr[i]
+		c = col + dc[i]
+
+		if r < 0 or c < 0:
+		    continue
+		if r >= len(matrix) or c >= len(matrix[0]): continue
+		if matrix[r][c] == 0:
+		    continue
+			
+		neighbors.append([r, c])
+
+	return neighbors
