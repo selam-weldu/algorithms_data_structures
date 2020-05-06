7607553b9f8e032b7646c29cb31f6730c5fec639
Selam Weldu <ssweldu@gmail.com>
2020-05-06 16:24:07 -0400

    Implement branch sums

--- leet_code/python/binary_trees/branch_sums.py
+++ leet_code/python/binary_trees/branch_sums.py
@@ -1,17 +1,17 @@
 # O(n) time | O(n) space
 def branchSums(root):
     sums = []
-    add(root, 0, sums)
-    return sums
+	add(root, 0, sums)
+	return sums
 
 
 def add(node, current_sum, sums):
 	if not node:
 		return
-
-	running_sum = current_sum + node.value
+		
+	current_sum += node.value
 	if not node.left and not node.right:
-		sums.append(running_sum)
+		sums.append(current_sum)
 
-	add(node.left, running_sum, sums)
-	add(node.right, running_sum, sums)
+	add(node.left, current_sum, sums)
+	add(node.right, current_sum, sums)
