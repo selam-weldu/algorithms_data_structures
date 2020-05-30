0cfec67dc5e363d80916100fb48301947054c3a3
Selam Weldu <ssweldu@gmail.com>
2020-05-30 16:30:47 -0400

    Implement first bad version

--- leet_code/python/searching/first_bad_version.py
+++ leet_code/python/searching/first_bad_version.py
@@ -6,16 +6,15 @@
 # O(log n) time | O(1) space
 class Solution:
     def firstBadVersion(self, n):
-        return self.check_version(0, n)
-
-    def check_version(self, low, high):
         bad_version = None
-
+        low = 0
+        high = n
         while low <= high:
-            mid = (low + high) // 2
+            mid = (low + (high - low)) // 2
             if isBadVersion(mid):
                 bad_version = mid
                 high = mid - 1
             else:
                 low = mid + 1
         return bad_version
+        
