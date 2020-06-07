4c2f7c0116852c48c921cbe282203f90038f4687
Selam Weldu <ssweldu@gmail.com>
2020-06-07 18:49:54 -0400

    Implement product of array without self

--- leet_code/python/arrays/products_without_self.py
+++ leet_code/python/arrays/products_without_self.py
@@ -15,3 +15,26 @@ def array_of_array_products(arr):
     products[i] *= right_idx
    
   return products
+
+
+def array_of_array_products2(arr):
+    
+  if len(arr) <= 1:
+    return []
+  
+  left = arr[:]
+  left[0] = 1
+  right = arr[:]
+  right[-1] = 1
+  
+  for i in range(1,len(arr)):
+    left[i] = arr[i-1] * left[i-1]
+    
+  for i in reversed(range(len(arr)-1)):
+    right[i] = arr[i+1] * right[i+1]
+  
+  products = []
+  for i in range(len(arr)):
+    products.append(left[i] * right[i])
+   
+  return products
