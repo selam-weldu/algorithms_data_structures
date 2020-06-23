e632cb3917744e8cc777dd717b8af253aaa948a9
Selam Weldu <ssweldu@gmail.com>
2020-06-23 15:38:44 -0400

    Implement levenshtein distance

---
+++ leet_code/python/DP/levenshtein_distance.py
@@ -0,0 +1,13 @@
+# O(nm) time and space
+def levenshteinDistance(str1, str2):
+    edits = [[x for x in range(len(str2)+1)] for y in range(len(str1)+1)]
+	for i in range(1,len(str1)+1):
+		edits[i][0] = i
+	
+	for i in range(1,len(str1)+1):
+		for j in range(1,len(str2)+1):
+			if str1[i-1] == str2[j-1]:
+				edits[i][j] = edits[i-1][j-1]
+			else:
+				edits[i][j] = 1 + min(edits[i-1][j-1],edits[i][j-1],edits[i-1][j])
+	return edits[-1][-1]
